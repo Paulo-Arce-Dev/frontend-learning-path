da67f9d95aee3a278a4d21a601fcf497e4431975
Paulo Arce <codebypaulo@gmail.com>
2025-06-17 03:20:03 -0300

    agregando ejercicios de js - desafio de 30 dias

---
+++ exercises/30-days-javascript/01-day/01-solucion.js
@@ -0,0 +1,58 @@
+// Ejercicios: día 1
+
+// 1
+// los comentarios pueden hacer que el código sea legible
+
+// 2
+// Bienvenido a 30DíasDeJavascript
+
+// 3
+/* los comentarios pueden hacer
+ que el código sea legible, fácil de
+ reutilizar e informativo
+*/
+
+// 4 --> variable.js
+const firstName = "paulo";
+let edad = 23;
+let estudiante = true;
+let estadoCivil = undefined;
+let millonario = null;
+
+// 5 --> datatypes.js
+console.log(typeof firstName);
+console.log(typeof edad);
+console.log(typeof estudiante);
+console.log(typeof estadoCivil);
+console.log(typeof millonario);
+
+// 6
+let genero;
+let telefono;
+let direccion;
+let objetivo;
+
+// 7
+let musicaFavorita = "Deep House";
+let juegoFavorito = "Counter Strike 2";
+let yerba = "Canarias - serena";
+let problemaVista = "Atismatismo";
+
+// 8
+const miNombre = "Paulo";
+const apellido = "Arce";
+let miEstadoCivil = "soltero";
+let pais = "Argentina";
+let miEdad = 23;
+
+// 9
+let myName = "Paulo",
+  myLastName = "Arce",
+  maritalStatus = "Soltero",
+  myCountry = "Argentina",
+  myAge = 23;
+
+// 10
+let myCurrentAge = 23;
+let yourAge = 19;
+console.log(myCurrentAge, yourAge);
---
+++ exercises/30-days-javascript/02-day/lvl-1.js
@@ -0,0 +1,82 @@
+// Ejercicios día 2
+
+// Nivel 1
+
+// 1
+let challenge = "30 Days Of Javascript";
+
+// 2
+console.log(challenge);
+
+// 3
+let longitudTexto = challenge.length;
+console.log(longitudTexto);
+
+// 4
+let aMayusculas = challenge.toUpperCase();
+
+// 5
+let aMinusculas = challenge.toLowerCase();
+
+// 6
+let extraerPrimeraPalabra = challenge.substring(0, 2);
+console.log(extraerPrimeraPalabra);
+// 7
+let extraerFrase = challenge.substring(3);
+console.log(extraerFrase);
+
+// 8
+let incluyeScript = challenge.includes("Script");
+console.log(incluyeScript);
+
+// 9
+let dividirTexto = challenge.split("");
+console.log(dividirTexto);
+
+// 10
+let dividirEnPalabras = challenge.split(" ");
+console.log(dividirEnPalabras);
+
+// 11
+let tecnologias = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+let tecnologiasConvertidas = tecnologias.split(",");
+console.log(tecnologiasConvertidas);
+
+// 12
+let cambiarChallenge = challenge.replace("Javascript", "Python");
+console.log(cambiarChallenge);
+
+// 13
+let caracter15 = challenge.charAt(15);
+console.log(caracter15);
+
+// 14
+let caracterJ = challenge.charCodeAt(11);
+console.log(caracterJ);
+
+// 15
+let primeraA = challenge.indexOf("a");
+console.log(primeraA);
+
+// 16
+let ultimaA = challenge.lastIndexOf("a");
+console.log(ultimaA);
+
+// 17
+let;
+
+// 18
+
+// 19
+
+// 20
+
+// 21
+
+// 22
+
+// 23
+
+// 24
+
+// 25
